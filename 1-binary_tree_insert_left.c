13ddeaf1fca703b5a27a57136f9967631d72d146
d404cjebq <imdhaydhw@gmail.com>
2026-04-28 18:47:47 +0300

    add an update for a spoiled lefty child for the Binary Trees

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,4 +1,11 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_node - Creates a binary tree node
+ * @parent: Pointer to the parent node
+ * @value: Value to put in the new node
+ *
+ * Return: Pointer to the new node, or NULL on failure
+ */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
     binary_tree_t *lefty_child, *tmp;
@@ -6,8 +13,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
         return (NULL);
     lefty_child= malloc(sizeof(binary_tree_t));    
     if(lefty_child == NULL)
-        return (NULL);
-    
+        return (NULL); 
     if (parent->left != NULL)
     {
         tmp = parent->left;
