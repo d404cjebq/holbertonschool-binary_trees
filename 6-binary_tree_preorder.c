0e7667d7f065436f8f892e5e88b8a8622f288fd5
leen <leenalgraawi@gmail.com>
2026-04-29 18:49:41 +0300

    fix spelling errors

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -18,8 +18,8 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 if (tree == NULL)
 return;
 
-func(root->n);
-binary_tree_preorder(tree->left, &fuc);
-binary_tree_preorder(tree->right, &fuc);
+func(tree->n);
+binary_tree_preorder(tree->left, &func);
+binary_tree_preorder(tree->right, &func);
 
 }
