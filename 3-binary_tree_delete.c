5d7abf30b11a2144c03e6c05497581ade2bfdf7a
root <leenalgraawi@gmai.com>
2026-04-29 17:46:36 +0300

    deleting the hole tree by calling

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -8,5 +8,19 @@
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
+while (tree)
+{
 
+if (tree->left == NULL && tree->right == NULL)
+{
+free(tree);
+return;
+}
+
+if (tree->left != NULL)
+binary_tree_delete(tree->left);
+
+if (tree->right != NULL)
+binary_tree_delete(tree->right);
+}
 }
