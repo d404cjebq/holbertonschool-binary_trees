e5f934533ab8436749701cfa8a94d285c88ff963
d404cjebq <imdhaydhw@gmail.com>
2026-04-30 16:07:46 +0300

    add the depth and the header

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -16,5 +16,5 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 {
 if (tree == NULL || tree->parent == NULL)
 return (0);
-return ( binary_tree_depth (tree->parent) + 1 );
+return (binary_tree_depth(tree->parent) + 1);
 }
