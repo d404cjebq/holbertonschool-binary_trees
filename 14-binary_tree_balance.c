4c2711fe58cd4a5f5c7278b99b1ac383e54094ce
leen <leenalgraawi@gmail.com>
2026-05-02 06:08:03 +0300

    cheaking the balance by the height diff left - right

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,45 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_balance - Measures the balance factor of a binary tree
+ * @tree: Pointer to the root node of the tree to measure
+ *
+ * Description:
+ * The balance factor of a binary tree is defined as the height of the
+ * left subtree minus the height of the right subtree. A NULL tree has
+ * a balance factor of 0. This function recursively computes the height
+ * of each subtree to determine the balance factor.
+ *
+ * Return: The balance factor of the tree as an integer
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+if (tree == NULL)
+return (0);
+
+return ((int)binary_trees_height(tree->left) - (int)binary_trees_height(tree->right));
+}
+
+/**
+ * binary_trees_height - Measure the height of a binary tree
+ * @tree: Pointer to the root node of the tree to measure
+ *
+ * Description:
+ * Computes the height of a binary tree. The height is defined as the
+ * number of edges on the longest path from the root node down to a leaf.
+ * If @tree is NULL, the height is considered to be 0.
+ *
+ * Return:
+ * The height of the tree as a size_t value.
+ */
+size_t binary_trees_height(const binary_tree_t *tree)
+{
+size_t l = 0, r = 0;
+if (tree == NULL)
+return (-1);
+
+l = tree->left ? binary_trees_height(tree->left) + 1 : 0;
+r = tree->right ? binary_trees_height(tree->right) + 1 : 0;
+
+return (l > r ? l : r);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -36,5 +36,15 @@ size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
-
+size_t binary_tree_siz(const binary_tree_t *tree);
+size_t binary_tree_leave(const binary_tree_t *tree);
+int binary_tree_balance(const binary_tree_t *tree);
+size_t binary_trees_height(const binary_tree_t *tree);
+int binary_tree_is_full(const binary_tree_t *tree);
+int binary_tree_is_perfect(const binary_tree_t *tree);
+int height(const binary_tree_t *tree);
+int perfect_size(int n);
+int size(const binary_tree_t *tree);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 #endif
